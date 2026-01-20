c083c33a055f023f042be80619708f51bca0dc96
LouisChabanon <louis@chabanon.es>
2026-01-21 00:10:37 +0100

    added logging integration to Sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -7,11 +7,17 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://0c36721275dc5f8cb1c94accd0ad2873@o4510447234711552.ingest.de.sentry.io/4510693773672528",
 
+  enabled: process.env.NODE_ENV === "production",
+
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-
+  integrations: [
+    // send console.log, console.warn, and console.error calls as logs to Sentry
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
   // Enable logs to be sent to Sentry
   enableLogs: true,
+  
 
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
