31342b76b58baad71ec30cff10e4bb748658526f
LouisChabanon <louis@chabanon.es>
2026-01-21 00:17:20 +0100

    Added logging to client and added mask for session replays

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -8,7 +8,9 @@ Sentry.init({
   dsn: "https://0c36721275dc5f8cb1c94accd0ad2873@o4510447234711552.ingest.de.sentry.io/4510693773672528",
 
   // Add optional integrations for additional features
-  integrations: [Sentry.replayIntegration()],
+  integrations: [Sentry.replayIntegration({
+    mask: [".mask-me"]
+  }), Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] })],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
